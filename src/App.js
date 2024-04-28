1e07c436a398f2a49fa05c9cacc6726c729ef66a
fathimaththasneem <fathimaththasnee@gmail.com>
2024-04-28 12:57:43 +0530

    responsive code added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import './App.css';
-import './pages/task.css'
-import { Images } from './db/Images';
+import "./App.css";
+import "./styles/utility.css"
+import "./pages/task.css";
+import { Images } from "./db/Images";
 
-import Task from './pages/Task';
-import Home from './pages/Home';
-import { useBrowser } from './context/Browsercontext';
-import { useEffect } from 'react';
+import Task from "./pages/Task";
+import Home from "./pages/Home";
+import { useBrowser } from "./context/Browsercontext";
+import { useEffect } from "react";
 
-const index = Math.floor(Math.random()*Images.length)
+const index = Math.floor(Math.random() * Images.length);
 const bgimage = Images[index].image;
 function App() {
-  const {name,browserdispatch}=useBrowser();
- 
-useEffect(()=>{
-    const userName=localStorage.getItem("name");
+  const { name, browserdispatch } = useBrowser();
 
-    browserdispatch({type:"NAME",payload:userName});
-  },[])
-  
+  useEffect(() => {
+    const userName = localStorage.getItem("name");
+
+    browserdispatch({ type: "NAME", payload: userName });
+  }, []);
+
   return (
-    <div className="app" 
-    style={{
-      backgroundImage: `radial-gradient(rgba(0, 0, 0,0.2), rgba(0, 0, 0,0.10)), url(${bgimage})`
-    }}>
-{  name?<Task/>:<Home/>}
-    
-
+    <div
+      className="app"
+      style={{
+        backgroundImage: `radial-gradient(rgba(0, 0, 0,0.2), rgba(0, 0, 0,0.10)), url(${bgimage})`,
+      }}
+    >
+      {name ? <Task /> : <Home />}
     </div>
   );
 }
